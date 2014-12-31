2c1eae4a85b52da9dac99497e1b7376b8ecadc9f
Steve Bohlen <sbohlen@gmail.com>
2014-12-31 14:26:05 -0500

    initial solution and rough code flow concept

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+
+            var instance = new MyClass();
+
+            var retry = new Retry();
+
+            var result = retry.Invoke(() => instance.IntReturningMethod(1, "func invoked"));
+
+            Assert.That(result, Is.EqualTo(1));
+            Assert.That(instance.IntResult, Is.EqualTo(1));
+            Assert.That(instance.StringResult, Is.EqualTo("func invoked"));
+
+            retry.Invoke(() => instance.VoidReturningMethod(2, "action invoked"));
+
+            Assert.That(instance.IntResult, Is.EqualTo(2));
+            Assert.That(instance.StringResult, Is.EqualTo("action invoked"));
+
+            Assert.That(instance.IntReturnInvokeCount, Is.EqualTo(1));
+            Assert.That(instance.VoidReturnInvokeCount, Is.EqualTo(2));
+
+        }
+    }
+
+
+    class MyClass
+    {
+        public string StringResult;
+        public int IntResult;
+        public int IntReturnInvokeCount;
+        public int VoidReturnInvokeCount;
+
+
+        public int IntReturningMethod(int theInt, string theString)
+        {
+            IntReturnInvokeCount++;
+            SetProperties(theInt, theString);
+            return theInt;
+        }
+
+        private void SetProperties(int theInt, string theString)
+        {
+            StringResult = theString;
+            IntResult = theInt;
+        }
+
+        public void VoidReturningMethod(int theInt, string theString)
+        {
+            VoidReturnInvokeCount++;
+            SetProperties(theInt, theString);
+        }
+    }
+
+    public class Retry
+    {
+        public TReturn Invoke<TReturn>(Func<TReturn> func)
+        {
+            return func.Invoke();
+        }
+
+        public void Invoke(Action action)
+        {
+            action.Invoke();
+            action.Invoke();
+        }
+    }
+}
