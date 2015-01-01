18351d35e9cd8b42c89c31e434167569caaf14d2
Steve Bohlen <sbohlen@gmail.com>
2015-01-01 08:58:33 -0500

    initial pass at permitting configuration of retriable exception list

--- src/Proteus.Retry/RetryPolicy.cs
+++ src/Proteus.Retry/RetryPolicy.cs
@@ -1,27 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Proteus.Retry
 {
     public class RetryPolicy
     {
         private int _maxRetries;
+        private IList<Type> _retriableExceptions = new List<Type>();
 
         public int MaxRetries
         {
             get { return _maxRetries; }
             set
             {
-                AssertValueIsAcceptable(value, arg => arg >= 0, new ArgumentOutOfRangeException("MaxRetries", "MaxRetries must be >= 0!"));
+                ThrowOnInvalidValue(value, arg => arg >= 0, new ArgumentOutOfRangeException("MaxRetries", "MaxRetries must be >= 0!"));
                 _maxRetries = value;
             }
         }
 
-        private void AssertValueIsAcceptable<TValue>(TValue value, Func<TValue, bool> isValidFunc, Exception exception) 
+        public IEnumerable<Type> RetriableExceptions
+        {
+            get { return new ReadOnlyCollection<Type> (_retriableExceptions); }
+        }
+
+        private void ThrowOnInvalidValue<TValue>(TValue value, Func<TValue, bool> isValidFunc, Exception exception) 
         {
             if (!isValidFunc.Invoke(value))
             {
                 throw exception;
             }
+        }
+
+        public void AddRetriableException<TException>() where TException: Exception
+        {
+            _retriableExceptions.Add(typeof(TException));
         }
     }
 }
---
+++ test/Proteus.Retry.Test/WhenRetryPolicyAddsRetriableExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Proteus.Retry.Test
+{
+    [TestFixture]
+    public class WhenRetryPolicyAddsRetriableExceptions
+    {
+        [Test]
+        public void ExceptionsAreTracked()
+        {
+            var policy = new RetryPolicy();
+
+            policy.AddRetriableException<Exception>();
+            policy.AddRetriableException<ArgumentOutOfRangeException>();
+
+            Assert.That(policy.RetriableExceptions, Has.Member(typeof(Exception)));
+            Assert.That(policy.RetriableExceptions, Has.Member(typeof(ArgumentOutOfRangeException)));
+        }   
+    }
+
+    
+}
