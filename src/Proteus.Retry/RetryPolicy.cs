7cedecdc74cd11539e31e64f376fafac8910f4ef
Steve Bohlen <sbohlen@gmail.com>
2015-01-01 10:50:39 -0500

    adjust RetryPolicy to properly respect superclass registrations for RetriableExceptions

--- src/Proteus.Retry/RetryPolicy.cs
+++ src/Proteus.Retry/RetryPolicy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Linq.Expressions;
 
 namespace Proteus.Retry
 {
@@ -40,12 +41,15 @@ namespace Proteus.Retry
 
         public bool IsRetriableException<TException>() where TException : Exception
         {
-            return _retriableExceptions.Contains(typeof (TException));
+            return _retriableExceptions.Contains(typeof(TException)) ||
+                   _retriableExceptions.Any(registeredException => typeof(TException).IsSubclassOf(registeredException));
+
         }
 
         public bool IsRetriableException(Exception exception)
         {
-            return _retriableExceptions.Contains(exception.GetType());
+            return _retriableExceptions.Contains(exception.GetType()) || 
+                   _retriableExceptions.Any(registeredException => exception.GetType().IsSubclassOf(registeredException));
 
         }
     }
--- test/Proteus.Retry.Test/WhenRetryPolicyAddsRetriableExceptions.cs
+++ test/Proteus.Retry.Test/WhenRetryPolicyAddsRetriableExceptions.cs
@@ -12,10 +12,10 @@ namespace Proteus.Retry.Test
         {
             var policy = new RetryPolicy();
 
-            policy.AddRetriableException<Exception>();
+            policy.AddRetriableException<ArithmeticException>();
             policy.AddRetriableException<ArgumentOutOfRangeException>();
 
-            Assert.That(policy.RetriableExceptions, Has.Member(typeof(Exception)));
+            Assert.That(policy.RetriableExceptions, Has.Member(typeof(ArithmeticException)));
             Assert.That(policy.RetriableExceptions, Has.Member(typeof(ArgumentOutOfRangeException)));
         }
 
@@ -29,7 +29,7 @@ namespace Proteus.Retry.Test
             var retry = new Retry(policy);
             var instance = new RetriableExceptionsTestSpy();
 
-            Assert.Throws<ArithmeticException>(() => retry.Invoke(() => instance.ThrowException<ArithmeticException>()));
+            Assert.Throws<ExpectableTestExecption>(() => retry.Invoke(() => instance.ThrowException<ExpectableTestExecption>()));
 
         }
 
@@ -38,9 +38,9 @@ namespace Proteus.Retry.Test
         {
             var policy = new RetryPolicy();
 
-            policy.AddRetriableException<ArithmeticException>();
+            policy.AddRetriableException<ExpectableTestExecption>();
 
-            Assert.That(policy.IsRetriableException<ArithmeticException>(), Is.True);
+            Assert.That(policy.IsRetriableException<ExpectableTestExecption>(), Is.True);
             Assert.That(policy.IsRetriableException<Exception>(), Is.False);
         }
 
@@ -49,13 +49,30 @@ namespace Proteus.Retry.Test
         {
             var policy = new RetryPolicy();
 
-            policy.AddRetriableException<ArithmeticException>();
+            policy.AddRetriableException<ExpectableTestExecption>();
 
-            Assert.That(policy.IsRetriableException(new ArithmeticException()), Is.True);
+            Assert.That(policy.IsRetriableException(new ExpectableTestExecption()), Is.True);
             Assert.That(policy.IsRetriableException(new Exception()), Is.False);
         }
 
-       
+        [Test]
+        public void CanReportDereivedExceptionTypeAsRetriable()
+        {
+            var policy = new RetryPolicy();
+
+            policy.AddRetriableException<Exception>();
+
+            Assert.That(policy.IsRetriableException<ExpectableTestExecption>(), Is.True);
+        }
+
+        [Test]
+        public void CanReportDerivedExceptionInstanceAsRetriable()
+        {
+            var policy = new RetryPolicy();
+
+            policy.AddRetriableException<Exception>();
+            Assert.That(policy.IsRetriableException(new ExpectableTestExecption()), Is.True);
+        }
 
         private class RetriableExceptionsTestSpy
         {
