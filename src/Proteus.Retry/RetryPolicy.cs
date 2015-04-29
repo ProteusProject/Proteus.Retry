f073c0ae34b0469abea08dd499371ada4c1a28a5
Steve Bohlen <sbohlen@gmail.com>
2015-04-28 20:02:10 -0400

    add missing XML comment for new ToString override

--- src/Proteus.Retry/RetryPolicy.cs
+++ src/Proteus.Retry/RetryPolicy.cs
@@ -164,6 +164,10 @@ namespace Proteus.Retry
             }
         }
 
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String" /> that represents this instance.</returns>
         public override string ToString()
         {
             var builder = new StringBuilder(base.ToString());
