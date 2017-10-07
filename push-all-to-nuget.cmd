79a1234a25ef8658ec332fba1d5419ae7dcd834b
sbohlen <sbohlen@gmail.com>
2017-10-07 05:14:26 -0400

    adding convenience script to push output RELEASE packages to NuGet

---
+++ push-all-to-nuget.cmd
@@ -0,0 +1,17 @@
+@echo off
+echo *** Pushing all packages in this folder to NuGet.org ***
+echo Note: this assumes the APIKey has already been set on this computer.
+echo If not, run the following command to set the ApiKey:
+echo            Nuget.exe setApiKey [API_KEY]
+echo .
+for /f %%F in ('dir /b .\build\release\*.nupkg') DO CALL :INVOKE %%F
+GOTO :EOF
+
+:INVOKE
+echo .
+echo Processing file %1 ...
+echo .
+.\tools\NuGet\NuGet.exe push .\build\release\%1 -Source https://www.nuget.org/api/v2/package/
+echo .
+echo                       ...complete!
+GOTO :EOF
